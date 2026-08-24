032f3407057925c8a95b49710d4a6544ed2ca79e
gad-biton <gadb633@gmail.com>
2026-08-24 12:23:55 +0300

    added part_6

--- country_code.py
+++ country_code.py
@@ -2,21 +2,17 @@ def country_code():
     status = True
     while status:
         country_code= input("enter country code: ")
+        country_code = country_code.replace(" ","").upper()
         if len(country_code) != 2:
             print(" enter only country code ! ")
             continue
         if country_code == "":
             print(" the country code cannot be empty ! ")
             continue
-        if country_code == "US":
-            if len(country_code) != 2:
-                print(" enter only country code ! ")
-                continue
-            else:
-                country_code_inside_US = input("enter country code inside US: ")
-                country_code_inside_US = country_code_inside_US.replace(" ","").upper()
-                status = False
-                return country_code_inside_US
-        country_code = country_code.replace(" ","").upper()
         status = False 
         return country_code
+def state_country(country_code):
+    if country_code == "US":
+        country_code_inside_US = input("enter country code inside US: ")
+        country_code_inside_US = country_code_inside_US.replace(" ","").upper()
+        return country_code_inside_US
--- weather.py
+++ weather.py
@@ -4,7 +4,15 @@ import os
 import json
 from datetime import datetime, timedelta
 import csv
+from name_city import *
+from country_code import *
 def get_key():
     load_dotenv()
     SECRET_KEY = os.getenv('API_key')
     return SECRET_KEY
+def get_location(city, country, state=""):
+    limit = 1        
+    r = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={limit}&appid={get_key()}")
+    req = r.json()
+    location = [req[0]["lat"],req[0]["lon"]]
+    return location 
