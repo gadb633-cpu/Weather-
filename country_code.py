405ca5d590dccb51b12664e36657e7d9c03dc438
gad-biton <gadb633@gmail.com>
2026-08-24 11:25:09 +0300

    added part_6

--- country_code.py
+++ country_code.py
@@ -1,13 +1,22 @@
 def country_code():
     status = True
     while status:
-        country_code= input("enter name city: ")
+        country_code= input("enter country code: ")
         if len(country_code) != 2:
             print(" enter only country code ! ")
             continue
         if country_code == "":
             print(" the country code cannot be empty ! ")
             continue
+        if country_code == "US":
+            if len(country_code) != 2:
+                print(" enter only country code ! ")
+                continue
+            else:
+                country_code_inside_US = input("enter country code inside US: ")
+                country_code_inside_US = country_code_inside_US.replace(" ","").upper()
+                status = False
+                return country_code_inside_US
         country_code = country_code.replace(" ","").upper()
         status = False 
         return country_code
