2b05a9395824eac68e85bebc5ad2fe0cf78c8f3c
gad-biton <gadb633@gmail.com>
2026-08-24 16:13:14 +0300

    fixed part_16

--- Process_data.py
+++ Process_data.py
@@ -1,8 +1,10 @@
 from weather import *
 from country_code import *
 from name_city import *
+from datetime import datetime
 def process_weather_data(location, weather):
     data = {
+    "search_time": datetime.now(),   
     "city": location["city"],
     "state": location["state"],
     "country": location["country"],
@@ -13,5 +15,5 @@ def process_weather_data(location, weather):
     "wind_speed":weather["wind"]["speed"]}
     return data
 def print_weather(weather_result):
-    print(f"city: {weather_result["city"]}\nstate: {weather_result["state"]}\ncountry: {weather_result["country"]}\ntemperature: {weather_result["temperature"]}\nfeels_like: {weather_result["feels_like"]}\ncondition: {weather_result["condition"]}\nhumidity: {weather_result["humidity"]}\nwind_speed: {weather_result["wind_speed"]}")
+    print(f"search_time: {weather_result["search_time"]}\ncity: {weather_result["city"]}\nstate: {weather_result["state"]}\ncountry: {weather_result["country"]}\ntemperature: {weather_result["temperature"]}\nfeels_like: {weather_result["feels_like"]}\ncondition: {weather_result["condition"]}\nhumidity: {weather_result["humidity"]}\nwind_speed: {weather_result["wind_speed"]}")
 
