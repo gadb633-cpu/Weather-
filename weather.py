37cec5c2f9c308134218718eb4349ca361772709
gad-biton <gadb633@gmail.com>
2026-08-24 18:03:03 +0300

    fixed get location

--- weather.py
+++ weather.py
@@ -7,10 +7,18 @@ from name_city import *
 from country_code import *
 from key import *
 def get_location(city, country, state=""):     
-    req = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&appid={get_key()}").json()
+    r = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&appid={get_key()}")
+    try:
+        r.raise_for_status()
+    except requests.HTTPError:
+        if r.status_code == 401:
+            print("Invalid API key")
+        else:
+            print(r.req.status_code)  
+        return None    
+    req= r.json() 
     if req == []:
-        return []
-        
+        return []         
     location = {"lat":req[0]["lat"],"lon":req[0]["lon"]}
     return location  
 def get_weather(latitude, longitude):
