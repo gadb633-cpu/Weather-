4eb9f99c6bdaf94887fab9c04c9c982aaf6bdb81
gad-biton <gadb633@gmail.com>
2026-08-24 14:45:46 +0300

    added part_13

--- weather.py
+++ weather.py
@@ -10,11 +10,15 @@ def get_key():
     load_dotenv()
     SECRET_KEY = os.getenv('API_key')
     return SECRET_KEY
-def get_location(city, country, state=""):
-    limit = 1        
-    r = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit={limit}&appid={get_key()}")
-    req = r.json()
+def get_location(city, country, state=""):     
+    req = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&appid={get_key()}").json()
     if req == []:
         return "Location not found !"
-    location = [req[0]["lat"],req[0]["lon"]]
+    location = {"lat":req[0]["lat"],"lon":req[0]["lon"]}
     return location  
+def get_weather(latitude, longitude):
+    req = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&units=Metric&appid={get_key()}").json()
+    weather = {"temp_c":req["main"]["temp"]}
+    return weather
+# loc = get_location("Jerusalem","il")
+# print(get_weather(loc["lat"],loc["lon"]))
